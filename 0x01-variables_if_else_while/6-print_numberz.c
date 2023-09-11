a917bb3282e302fd73352b27ec65e4a6cef0aaef
Meggazord <k_megahed@hotmail.com>
2023-09-11 14:44:31 +0300

    modified

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,10 @@ int main(void)
 {
 int n = 0;
 while (n < 10)
-        {
-                putchar(48 + n);
-                n++;
-        }
+{
+	putchar(48 + n);
+	n++;
+}
 putchar('\n');
 return (0);
 }

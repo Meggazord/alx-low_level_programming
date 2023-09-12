46f8e244a3dbdebaad18ceebcc737b0c04c922f5
Meggazord <k_megahed@hotmail.com>
2023-09-13 00:16:04 +0300

    modified

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * main - check if character is absolute value
- * Return: i
- */
+* _abs - check if character is absolute value
+* @i: check value of i
+* Return: i
+*/
 int _abs(int i)
 {
 	if (i < 0)

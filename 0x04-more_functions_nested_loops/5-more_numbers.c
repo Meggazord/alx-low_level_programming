bd4184e60e24810573334c504ccd251a06c3e7e9
Meggazord <k_megahed@hotmail.com>
2023-09-15 01:12:37 +0300

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * more_numbers - prints 0 to 14 10 times
  * Return:0
  */
-
 void more_numbers(void)
 {
 	int i, n;
@@ -19,5 +17,4 @@ void more_numbers(void)
 		}
 	}
 	_putchar('\n');
-
 }

8062cc569299f09c2000c688e0ae6fd95e943f6f
Meggazord <k_megahed@hotmail.com>
2023-09-15 01:05:32 +0300

    fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,10 @@ void print_most_numbers(void)
 {
 	char i;
 
-	for(i = '0'; i < '10'; i++)
+	for(i = 0; i < 10; i++)
 	{
-		if (i != '2' &&  i != '4')
-			_putchar('0' + n);
+		if (i != 2 &&  i != 4)
+			_putchar(48 + n);
 
 	}
 	_putchar('\n');

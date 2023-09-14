9ad627ffdde181adcf9252c71f7d064eb2adf405
Meggazord <k_megahed@hotmail.com>
2023-09-15 01:22:08 +0300

    fixed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,15 +10,15 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if(n > 0)
-	{ 
+	if (n > 0)
+	{
 		while (i < n)
 		{
 			_putchar('_');
 			i++;
 		}
 		_putchar('\n');
-	}	
+	}
 	else
 		_putchar('\n');
 }

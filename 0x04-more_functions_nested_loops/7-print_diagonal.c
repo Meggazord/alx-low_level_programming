35b6280bf4392e2d70e8c886319358415171f1e1
Meggazord <k_megahed@hotmail.com>
2023-09-15 01:38:48 +0300

    fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 
 void print_diagonal(int n)
 {
-	if (i <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -20,7 +20,9 @@ void print_square(int size)
 	for (y = 0; y < size; y++)
 	{
 		for (x = 0; x < size; x++)
+		{
 			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }

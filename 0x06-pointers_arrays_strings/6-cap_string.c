7443b40b0ab404fccffc18e243532219677e4836
Meggazord <k_megahed@hotmail.com>
2023-09-22 02:19:02 +0300

    fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,37 +5,25 @@
  * @str: Pointer to the input string
  * Return: Pointer to the modified string
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i = 0;
-	int new_word;
 
-	if (str[i] >= 'a' && str[i] <= 'z')
+	while (s[i] != '\0')
 	{
-		str[i] = str[i] - 32;
-	}
-
-	i++;
-
-	while (str[i] != '\0')
-	{
-		if (i == 0 || str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-				str[i] == '}')
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			new_word = 1;
-		}
-		else if (new_word)
-		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+					s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+					s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+					s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+					s[i - 1] == '}')
 			{
-				str[i + 1] = str[i + 1] - 32;
+				s[i] = s[i] - 32;
 			}
-			new_word = 0;
 		}
 		i++;
 	}
-	return (str);
+
+	return (s);
 }

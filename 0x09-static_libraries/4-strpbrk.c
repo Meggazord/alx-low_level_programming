fe36a323776720318775be19c0deaf9dcccc7fa8
Meggazord <k_megahed@hotmail.com>
2023-10-07 23:53:48 +0300

    task 9

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: Pointer to the string to be searched
+ * @accept: Pointer to the string containing accepted characters
+ * Return: Pointer to the first byte in s matching  one of the bytes in accept
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+				return (s + x);
+		}
+	}
+	return ('\0');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _abs - check if character is absolute value
+* @i: check value of i
+* Return: i
+*/
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}

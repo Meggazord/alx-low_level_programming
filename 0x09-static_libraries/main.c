9f062aba9568d378c5cb638c794f0c237e76fb6e
Meggazord <k_megahed@hotmail.com>
2023-10-08 02:33:34 +0300

    fixed

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This program demonstrates the usage of the _puts function to print a quote
+ * to the standard output. The quote is by Kevin Mitnick, a renowned computer
+ * security consultant and hacker. The quote is enclosed within double quotes
+ * and includes both single and double quotation marks. After printing the
+ * quote to the standard output, the program returns 0, indicating successful
+ * execution.
+ *
+ * Return: 0 on successful execution
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+	"\t- Kevin Mitnick");
+	return (0);
+}

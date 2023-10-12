953dd6edb73c2b43488289dd6347ce86ae1ec3ef
Meggazord <k_megahed@hotmail.com>
2023-10-12 23:44:36 +0300

    done

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,22 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * struct format_specifier - Struct to match format specifiers with functions
+ * @specifier: The format specifier
+ * @f: The corresponding function to handle the specifier
+ */
+typedef struct format_specifier
+{
+	char *specifier;
+	void (*f)(va_list);
+} format_t;
+
+/* Function prototypes */
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */

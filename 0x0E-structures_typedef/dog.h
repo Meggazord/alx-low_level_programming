c501420835349251642c73667f186289f563bb0e
Meggazord <k_megahed@hotmail.com>
2023-10-12 19:05:00 +0300

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,29 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - declar Dog Struct
- * @name: name
- * @age: age
- * @owner: owner
+ * struct dog - Dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  * Desc: header file
  */
-
-struct dog
+typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-}
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
+char *name;
+float age;
+char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 
-#endif /* DOG_H */
+#endif

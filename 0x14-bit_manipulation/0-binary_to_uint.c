60d65747a30745a735ee0723ea92f6d27caf2eef
Meggazord <k_megahed@hotmail.com>
2023-10-26 23:24:07 +0200

    fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,14 +15,14 @@ unsigned int binary_to_uint(const char *b)
 
 	sum = 0;
 
-	if (b == NULL)
+	if (*b == NULL)
 		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		sum = sum * 2 + (b[i] - '0');
+		sum = (sum * 2) + (b[i] - '0');
 	}
 
 	return (sum);

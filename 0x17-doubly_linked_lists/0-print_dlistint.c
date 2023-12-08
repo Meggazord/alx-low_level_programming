3bf5b16d17984aeb24773798c0052e41da2f9f1e
Meggazord <k_megahed@hotmail.com>
2023-12-08 03:21:53 +0200

    fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,16 +9,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count;
+	size_t count;
 
-    count = 0;
+	count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    return (count);
+	return (count);
 }

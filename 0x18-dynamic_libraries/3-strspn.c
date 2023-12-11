8e43420e974e5a6c961efe32cdf365b82f66c3b9
Meggazord <k_megahed@hotmail.com>
2023-12-11 17:34:16 +0200

    new taks

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: Pointer to the string to be searched
+ * @accept: Pointer to the string containing accepted characters
+ * Return: number of bytes in initial segment of s consisting only of accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		y = 0;
+
+		for (; accept[y] != s[x]; y++)
+		{
+			if (accept[y] == '\0')
+				return (x);
+		}
+	}
+
+	return (x);
+}

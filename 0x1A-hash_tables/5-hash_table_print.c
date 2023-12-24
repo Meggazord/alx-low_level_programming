7612cc7392f6927b75623b420589a371eee5e2fa
Meggazord <k_megahed@hotmail.com>
2023-12-24 23:52:09 +0200

    fixed

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table to be printed
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    hash_node_t *temp;
+    unsigned long int i;
+    int flag;
+
+    if (ht == NULL)
+        return;
+
+    flag = 0;
+
+    printf("{");
+
+    for (i = 0; i < ht->size; i++)
+    {
+        temp = ht->array[i];
+
+        while(temp != NULL)
+        {
+            if (flag == 1)
+                printf(", ");
+            printf("'%s': '%s'", temp->key, temp->value);
+            flag = 1; 
+            temp = temp->next;
+        }
+    }
+    printf("}\n");
+
+}
